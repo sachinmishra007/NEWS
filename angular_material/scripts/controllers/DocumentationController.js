a39caa5adc35c48bae7c0037965595988c1f0138
sachinmishra007 <sachinmishra609@gmail.com>
2016-12-31 14:11:48 +0530

    Add files via upload

--- angular_material/scripts/controllers/DocumentationController.js
+++ angular_material/scripts/controllers/DocumentationController.js
@@ -1,10 +1,10 @@
 ﻿
 
 angular.module("NewsAPI.controllers", [])
-.controller("DocumentationController", function ($scope, $log, $window, $timeout, $interval, $document, NewsAPIServices) {
+.controller("DocumentationController", function ($scope, $log, $window, $timeout, $interval, $document, NewsAPIServices, $mdToast) {
     $scope.IsNewsVisible = true;
     $scope.ResultData = {};
-
+    $scope.HidePanel = false;
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -57,6 +57,7 @@ angular.module("NewsAPI.controllers", [])
 
         });
     }
+
     WelcomeScreenAnimation('bounceInDown');
 
     $timeout(function () {
@@ -82,23 +83,80 @@ angular.module("NewsAPI.controllers", [])
         $window.open('https://www.sololearn.com/Profile/3059838#', '_blank');
     }
 
-    $scope.SearchTextForCountry = function (_placeName) {
-        var _urla = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + _placeName + "')";
-        
-        $scope.IsNewsVisible = false;
-        NewsAPIServices
-       .GetWeatherInformation(_urla)
-       .success(function (_result) {
-           $scope.ResultData = _result.query.results.channel;           
-           $scope.IsNewsVisible = true;
-           $log.info("Loaded");
-       })
-       .error(function (_error) {
-           $log.info(_error);
-       });
+    $scope.animateForecast = function (x) {
 
+
+        $('#Span14').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            //$(this).removeClass();
+        });
+
+
+        $('#Span1,#Span2,#Span3,#Span4,#Span5').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            //$(this).removeClass();
+        });
+
+        $timeout(function () {
+            x = 'zoomInRight';
+            $('#Span6,#Span7,#Span8,#Span9').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                //$(this).removeClass();
+            });
+            x = 'bounceInDown';
+            $('#Span10,#Span11,#Span12,#Span13').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                //$(this).removeClass();
+            });
+
+            x = 'zoomInRight';
+            $('#span16').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                //$(this).removeClass();
+            });
+        }, 200);
+
+
+
+
+    }
+
+    $scope.SearchTextForCountry = function (_placeName) {
+
+        if (_placeName == "" || !angular.isDefined(_placeName)) {
+
+            var Toast = $mdToast.simple()
+                        .textContent("Please Valid State Name")
+                        .action('Okay')
+                        .highlightAction(true);
+
+            $mdToast.show(Toast).then(function (response) {                
+                if (response == "ok") {
+                    $scope.HidePanel = false;
+                }
+            });
+
+
+        }
+        else {
+
+            var _urla = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + _placeName + "')";
+            $scope.IsNewsVisible = false;
+            NewsAPIServices
+           .GetWeatherInformation(_urla)
+           .success(function (_result) {
+               $scope.ResultData = _result.query.results.channel;
+               $scope.IsNewsVisible = true;
+               $scope.HidePanel = true;
+               $scope.animateForecast('zoomInLeft');
+               //$log.info($scope.ResultData);
+           })
+           .error(function (_error) {
+               $log.info(_error);
+           });
+        }
     };
 
+    $scope.TextChangesEvent = function (stringValue) {        
+        if (stringValue.length == 0) {
+            $scope.HidePanel = false;
+        }
+    };
 
 
 
